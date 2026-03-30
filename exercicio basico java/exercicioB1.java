384984360004456c3e06d1aee2b229bec5345fc4
Artur Maximo <artur_fonseca@outlook.com>
2026-03-30 09:15:02 -0300

    exercicio basico java 1/20 - data 30/03

---
+++ exercicio basico java/exercicioB1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class exercicioB1
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    System.out.println("Digite um numero: ");
+	    int numero = sc.nextInt();
+	    
+	    for (int i = 0;i <= numero; i++){
+	        System.out.println("Numero: "+i);
+	    }
+	    
+	}
+}
